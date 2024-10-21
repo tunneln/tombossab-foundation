f88cc4c2c3ec86cd5adfb0868f744f87bd794a04
Noel Negusse <Noel.Blob@gmail.com>
2024-10-21 19:48:43 -0400

    bridged connection between frontend and backend, so they can both be run via one jar

--- backend/src/main/java/org/tombossabfoundation/backend/controllers/HelloController.java
+++ backend/src/main/java/org/tombossabfoundation/backend/controllers/HelloController.java
@@ -1,11 +1,13 @@
 package org.tombossabfoundation.backend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
     
     @GetMapping("/hello") 

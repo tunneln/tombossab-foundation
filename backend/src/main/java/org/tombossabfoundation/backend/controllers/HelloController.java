4852fb30a59a2a07cc441de43b6415038605eb93
Noel Negusse <Noel.Blob@gmail.com>
2024-11-30 09:27:10 -0600

    fixed critical bug with direct routes hitting the backend instead of next.js

--- backend/src/main/java/org/tombossabfoundation/backend/controllers/HelloController.java
+++ backend/src/main/java/org/tombossabfoundation/backend/controllers/HelloController.java
@@ -1,18 +1,20 @@
 package org.tombossabfoundation.backend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
-@RequestMapping("/api")
 public class HelloController {
     
-    @GetMapping("/hello") 
+    @GetMapping("/api/hello") 
     public String sayHello() {
         return "Hello, World. The time is " + new Date();
     }
 
+    @GetMapping(value = "/{path:^(?!api).*}")
+    public String forwardToReact() {
+        return "forward:/";
+    }
 }

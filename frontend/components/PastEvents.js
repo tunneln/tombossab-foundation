56c492dd62d0e3ceedb19b9a7ed24d381f31968d
Noel Negusse <Noel.Blob@gmail.com>
2026-06-21 11:17:02 -0400

    Add Coffee & Women Empowerment event page

--- frontend/components/PastEvents.js
+++ frontend/components/PastEvents.js
@@ -24,6 +24,31 @@ const PastEvents = () => {
                         <div className="blog-content">
                             <div className="blog-item blog-item1">
                                 <div className="blog-img">
+                                    <Link href="/events-detail">
+                                        <img src="/images/img8.jpg" alt="Community Field Day" />
+                                    </Link>
+                            <span className="blog__tag blog__tag1">
+                                <span className="date__num-text">26</span>
+                                <span className="date__mon-text">Jul 2025</span>
+                            </span>
+                                </div>
+                                <div className="blog-inner-content">
+                                    <h3 className="blog__title">
+                                        <Link href="/events-detail">Community Field Day</Link>
+                                    </h3>
+                                    <ul className="blog__list">
+                                        <li className="blog__dot-active">3:30pm to 9:00pm &nbsp;&nbsp;</li>
+                                        <li className="blog__dot-active">Cottonwood Park &nbsp;</li>
+                                        <li className="blog__dot-active">Richardson, TX</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="blog-content">
+                            <div className="blog-item blog-item1">
+                                <div className="blog-img">
                                     <img src="/images/img7.jpg" alt="" />
                             <span className="blog__tag blog__tag2">
                                 <span className="date__num-text">30</span>
---
+++ frontend/pages/coffee-women-empowerment.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from "../components/Layout";
+import NavOne from "../components/NavOne";
+import PageHeader from "../components/PageHeader";
+import Footer from "../components/Footer";
+import CoffeeEventDetail from "../components/CoffeeEventDetail";
+
+const CoffeeWomenEmpowermentPage = () => {
+    return (
+        <Layout pageTitle="Tombossa B Foundation | Coffee & Women Empowerment">
+            <NavOne />
+            <PageHeader prev="Events" link="/events" title="Coffee & Women Empowerment" />
+            <CoffeeEventDetail />
+            <Footer />
+        </Layout>
+    );
+};
+
+export default CoffeeWomenEmpowermentPage;
